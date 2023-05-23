8a0fc7a2e87b97bedf388cdb6853fea4fc8179fb
wangmeng <wangmeng@i72.com>
2023-05-23 14:43:46 +0800

    feat: 增加默认参数

--- packages/elementUI/src/main.js
+++ packages/elementUI/src/main.js
@@ -1,7 +1,7 @@
 import rewriteContainer from "@/rewriteContainer.js"
 import { fixStyle, isInQianKun } from "@root/utils/index.js"
 
-function fixElementStyle({ container }, options) {
+function fixElementStyle({ container }, options = {}) {
   fixStyle({ container }, options)
 
   rewriteContainer()
