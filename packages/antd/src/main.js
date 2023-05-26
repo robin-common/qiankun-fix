ea49c46e00447256aef75c6e97dac9d00241b338
wangmeng <wangmeng@i72.com>
2023-05-26 08:57:13 +0800

    fix: 默认值问题

--- packages/antd/src/main.js
+++ packages/antd/src/main.js
@@ -2,7 +2,7 @@ import rewriteContainer from "@/rewriteContainer.js";
 import { fixStyle, isInQianKun } from "@root/utils";
 import actions from "@root/utils/actions";
 
-function fixElementStyle({ container }, options) {
+function fixElementStyle({ container }, options = { whiteList: [] }) {
   fixStyle({ container }, options);
 
   rewriteContainer(options);
